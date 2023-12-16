d7dc28d2dcf71f59022622b64062b81f73d820ac
pamelakelly0023 <pamela.kelly95@gmail.com>
2023-12-16 18:28:23 -0300

    configuracao gerar token jwt

--- ListaTarefas/Configuration/IdentityConfiguration.cs
+++ ListaTarefas/Configuration/IdentityConfiguration.cs
@@ -25,11 +25,37 @@ namespace ListaTarefas.Configuration
                 options.UseMySql(connectionString,
                 ServerVersion.AutoDetect(connectionString)));
 
+            //AddIdentity
             services.AddDefaultIdentity<IdentityUser>()
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
+            
+            // JWT
+            var appSettingsSection = configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
                 
+            services.AddAuthentication(x => {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x => {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidAudience = appSettings.ValidoEm,
+                    ValidIssuer = appSettings.Emissor,
+                    IssuerSigningKey = new SymmetricSecurityKey(key)
+                };
+            });
+
             return services;
         }
     }
---
+++ ListaTarefas/Controllers/AuthController.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using ListaTarefas.Controllers;
+using ListaTarefas.Extensions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ListaTarefas.Controllers
+{
+    [Route("api/[controller]")]
+	[ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly AppSettings _appSettings;
+        private readonly ILogger _logger;
+
+        public AuthController( 
+                              SignInManager<IdentityUser> signInManager, 
+                              UserManager<IdentityUser> userManager, 
+                              IOptions<AppSettings> appSettings,
+                              ILogger<AuthController> logger)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _logger = logger;
+            _appSettings = appSettings.Value;
+        }
+
+        [HttpGet]
+        public string GerarTokenJWT()
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var token = tokenHandler.CreateToken(new SecurityTokenDescriptor
+            {
+                Issuer = _appSettings.Emissor,
+                Audience = _appSettings.ValidoEm,
+                Expires = DateTime.UtcNow.AddHours(_appSettings.ExpiracaoHoras),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            });
+
+            var encodedToken = tokenHandler.WriteToken(token);
+            return encodedToken;
+        }
+    }
+}
